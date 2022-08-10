a7b407a9e7cb4f7f3613bf3da3fea59af64aa4e6
kirstgonz <104031234+kirstgonz@users.noreply.github.com>
2022-08-09 22:08:49 -0500

    added color coding for time blocks if past, present, or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,19 @@ setInterval(function() {
     checkTime();
 }, 1000);
 
+function checkTime() { 
+    for (let currentHourInt = 9; currentHourInt < 18; currentHourInt++){
+        let currentHour = moment().hour(currentHourInt);
+        let nextHour = moment().hour(currentHourInt + 1);
+        let textAreaId = '#' + currentHourInt + 'a';
+
+        if (moment().isBetween(currentHour, nextHour)){
+            $( textAreaId ).addClass('present');
+        } else if (moment().isAfter(nextHour)) {
+            $( textAreaId ).addClass('past');
+        } else {
+            $( textAreaId ).addClass('future');
+        }
+    }
+};
+
